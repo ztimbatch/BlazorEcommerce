3c4b2d4ccc6812ca1690097b3c030efca4ff2d05
ztimbatch <ztimbatch@gmail.com>
2022-06-25 22:43:34 +0400

    Administration for Products & Product Variants & select box for variants

--- BlazorEcommerce/Server/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Server/Services/ProductService/ProductService.cs
@@ -12,7 +12,7 @@
         {
             var response = new ServiceResponse<List<Product>>()
             {
-                Data = await _context.Products.ToListAsync()
+                Data = await _context.Products.Include(p => p.Variants).ToListAsync()
             };
 
             return response;
@@ -21,7 +21,10 @@
         public async Task<ServiceResponse<Product>> GetProductAsync(int productId)
         {
             var response = new ServiceResponse<Product>();
-            var product = await _context.Products.FindAsync(productId);
+            var product = await _context.Products
+                .Include(p => p.Variants)
+                .ThenInclude(v => v.ProductType)
+                .FirstOrDefaultAsync(p => p.Id == productId);
 
             if (product == null)
             {
@@ -34,6 +37,20 @@
             }
 
             return response;
+        }
+
+        public async Task<ServiceResponse<List<Product>>> GetProductsByCategory(string categoryUrl)
+        {
+            var response = new ServiceResponse<List<Product>>
+            {
+
+                Data = await _context.Products
+                       .Where(p => p.Category.Url.ToLower().Equals(categoryUrl.ToLower()))
+                       .Include(p => p.Variants)
+                       .ToListAsync()
+            };
+
+            return response;   
         }
     }
 }
